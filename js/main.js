5839db10eab0dd05dedd79e83c49b32cceefa897
Fabricio Moreno <fabricio.morenonaranjo@hotmail.com>
2021-05-10 19:29:23 -0500

    Created a message to user for insert text

--- js/main.js
+++ js/main.js
@@ -14,29 +14,49 @@ let secretingBtn = document.getElementById('btn-secreting')
 const DISPLAY_BOX = document.getElementById('result-box')
 const DISPLAY_BOX_CONTENT = document.getElementById('result-box_content')
 
-function isEmptyTextInput(input) {
-    const text = input.textContent
+function isEmptyTextInput(input,condition='input') {
+    let text
+    if(condition === 'input'){
+        text = input.textContent
+    }
+    else{
+        text = input
+    }
     const textWithoutSpace = text.replace(/^\s+/, '').replace(/\s+$/, '')
 
     return textWithoutSpace === '' ? true : false
  }
 
 function start(){
-    
-    //Delete all content when btn is pressed
-    DISPLAY_BOX_CONTENT.innerHTML = '' 
-    
-    //Encrypt input text
-    const textToEncrypt=  document.getElementById('textarea').value
-    const encryptor = new Encryptor()
-    
-    const textEncrypted = encryptor.encrypt(textToEncrypt);
-    showArrayIntoHtml(DISPLAY_BOX_CONTENT,textEncrypted)
-    
-    
-    //Test if exists content to create or delete clipboard btn
 
-    if(!isEmptyTextInput(DISPLAY_BOX_CONTENT)){
+    //Get input text to encrypt
+    const textToEncrypt=  document.getElementById('textarea').value
+
+    //Test if texarea is empty if is true show a message else
+    //active encryting mode
+
+    if(isEmptyTextInput(textToEncrypt,'text')){
+        //Show a message to user
+        DISPLAY_BOX_CONTENT.innerHTML = ''
+
+        DISPLAY_BOX_CONTENT.insertAdjacentHTML('beforeend','<p>Waiting your secret</p>')
+
+        //If exists a clipborad btn delete it
+        if(document.getElementById('clipboaard-btn')){
+            //Remove the clipboard box
+            const CLIPBOARD_BOX = document.getElementById('result-box_clipboard')
+            DISPLAY_BOX.removeChild(CLIPBOARD_BOX)
+        }
+    }
+    else
+    {
+        //Encrypting the text
+        DISPLAY_BOX_CONTENT.innerHTML = ''
+
+        const encryptor = new Encryptor()
+        const textEncrypted = encryptor.encrypt(textToEncrypt);
+        showArrayIntoHtml(DISPLAY_BOX_CONTENT,textEncrypted)
+
         //Test if not exists a clipboard btn to create one
 
         if(!document.getElementById('clipboaard-btn')){
@@ -49,14 +69,6 @@ function start(){
             //Add clipboard functionality to btn
             const CLIPBOARD_BTN = document.getElementById('clipboaard-btn')
             CLIPBOARD_BTN.addEventListener('click',()=>{clipboard(DISPLAY_BOX_CONTENT)})
-        }
-    }
-    else{
-        
-        if(document.getElementById('clipboaard-btn')){
-            //Remove the clipboard box
-            const CLIPBOARD_BOX = document.getElementById('result-box_clipboard')
-            DISPLAY_BOX.removeChild(CLIPBOARD_BOX)
         }
     }
 
